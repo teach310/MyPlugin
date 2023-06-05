c8999a2b9ad09d92f4fda3e14c7fb4095d1e8c98
teach310 <okms310@yahoo.co.jp>
2023-06-05 16:45:50 +0900

    first commit

---
+++ Assets/Sample/SampleScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("ここでプラグインを使う");
+    }
+}
