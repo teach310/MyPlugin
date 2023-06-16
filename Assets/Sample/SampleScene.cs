7e420a97aa8bf0295379fa33a9f2fe2a119b48f1
teach310 <okms310@yahoo.co.jp>
2023-06-16 21:39:11 +0900

    歩数を入れるためのUIを作成

--- Assets/Sample/SampleScene.cs
+++ Assets/Sample/SampleScene.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Sample;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SampleScene : MonoBehaviour
 {
+    [SerializeField] Text label;
+
     public void OnClick()
     {
-        Debug.Log("SamplePlugin.HelloWorld() = " + SamplePlugin.HelloWorld());
+        label.text = "ここに歩数が入る";
+        // Debug.Log("SamplePlugin.HelloWorld() = " + SamplePlugin.HelloWorld());
     }
 }
