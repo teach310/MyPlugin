4835f27f19df5b1fec89cab6eb5043e09afb55a3
teach310 <okms310@yahoo.co.jp>
2023-08-14 23:06:34 +0900

    update plugins

--- Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
+++ Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
@@ -260,6 +260,9 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import CoreBluetooth;
+@import Foundation;
+@import ObjectiveC;
 #endif
 
 #endif
@@ -280,6 +283,54 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class CBCentralManager;
+@class CBPeripheral;
+@class NSString;
+@class NSNumber;
+@class CBService;
+@class CBCharacteristic;
+
+SWIFT_CLASS("_TtC12SamplePlugin9BLESample")
+@interface BLESample : NSObject <CBCentralManagerDelegate, CBPeripheralDelegate>
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (void)centralManagerDidUpdateState:(CBCentralManager * _Nonnull)central;
+- (void)centralManager:(CBCentralManager * _Nonnull)central didDiscoverPeripheral:(CBPeripheral * _Nonnull)peripheral advertisementData:(NSDictionary<NSString *, id> * _Nonnull)advertisementData RSSI:(NSNumber * _Nonnull)RSSI;
+- (void)centralManager:(CBCentralManager * _Nonnull)central didConnectPeripheral:(CBPeripheral * _Nonnull)peripheral;
+- (void)centralManager:(CBCentralManager * _Nonnull)central didFailToConnectPeripheral:(CBPeripheral * _Nonnull)peripheral error:(NSError * _Nullable)error;
+- (void)centralManager:(CBCentralManager * _Nonnull)central didDisconnectPeripheral:(CBPeripheral * _Nonnull)peripheral error:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverServices:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverCharacteristicsForService:(CBService * _Nonnull)service error:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didUpdateValueForCharacteristic:(CBCharacteristic * _Nonnull)characteristic error:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didWriteValueForCharacteristic:(CBCharacteristic * _Nonnull)characteristic error:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic * _Nonnull)characteristic error:(NSError * _Nullable)error;
+@end
+
+
+SWIFT_CLASS("_TtC12SamplePlugin18CB4UCentralManager")
+@interface CB4UCentralManager : NSObject <CBCentralManagerDelegate>
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (void)centralManagerDidUpdateState:(CBCentralManager * _Nonnull)central;
+- (void)centralManager:(CBCentralManager * _Nonnull)central didDiscoverPeripheral:(CBPeripheral * _Nonnull)peripheral advertisementData:(NSDictionary<NSString *, id> * _Nonnull)advertisementData RSSI:(NSNumber * _Nonnull)RSSI;
+@end
+
+
+SWIFT_EXTERN BOOL cb4u_central_manager_is_scanning(void const * _Nonnull centralPtr) SWIFT_WARN_UNUSED_RESULT;
+
+
+SWIFT_EXTERN void * _Nonnull cb4u_central_manager_new(void) SWIFT_WARN_UNUSED_RESULT;
+
+
+SWIFT_EXTERN void cb4u_central_manager_register_handlers(void const * _Nonnull centralPtr, void (* _Nonnull didUpdateStateHandler)(void const * _Nonnull, int32_t), void (* _Nonnull didDiscoverPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull));
+
+
+SWIFT_EXTERN void cb4u_central_manager_release(void const * _Nonnull centralPtr);
+
+
+SWIFT_EXTERN void cb4u_central_manager_scan_for_peripherals(void const * _Nonnull centralPtr, char const * _Nullable const * _Nonnull serviceUUIDs, int32_t serviceUUIDsCount);
+
+
+SWIFT_EXTERN void cb4u_central_manager_stop_scan(void const * _Nonnull centralPtr);
+
 
 SWIFT_EXTERN int32_t sample_plugin_helloworld(void) SWIFT_WARN_UNUSED_RESULT;
 
