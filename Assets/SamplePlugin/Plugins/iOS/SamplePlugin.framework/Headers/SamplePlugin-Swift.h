75f275510f5e72ad0cd1d015bf52fafda11a4e3b
teach310 <okms310@yahoo.co.jp>
2023-08-18 22:04:34 +0900

    update plugins

--- Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
+++ Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
@@ -315,6 +315,7 @@ SWIFT_CLASS("_TtC12SamplePlugin18CB4UCentralManager")
 - (void)centralManager:(CBCentralManager * _Nonnull)central didFailToConnectPeripheral:(CBPeripheral * _Nonnull)peripheral error:(NSError * _Nullable)error;
 - (void)centralManager:(CBCentralManager * _Nonnull)central didDisconnectPeripheral:(CBPeripheral * _Nonnull)peripheral error:(NSError * _Nullable)error;
 - (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverServices:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverCharacteristicsForService:(CBService * _Nonnull)service error:(NSError * _Nullable)error;
 @end
 
 
@@ -327,7 +328,7 @@ SWIFT_EXTERN BOOL cb4u_central_manager_is_scanning(void const * _Nonnull central
 SWIFT_EXTERN void * _Nonnull cb4u_central_manager_new(void) SWIFT_WARN_UNUSED_RESULT;
 
 
-SWIFT_EXTERN void cb4u_central_manager_register_handlers(void const * _Nonnull centralPtr, void (* _Nonnull didUpdateStateHandler)(void const * _Nonnull, int32_t), void (* _Nonnull didDiscoverPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull), void (* _Nonnull didConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull), void (* _Nonnull didFailToConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDisconnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverServicesHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t));
+SWIFT_EXTERN void cb4u_central_manager_register_handlers(void const * _Nonnull centralPtr, void (* _Nonnull didUpdateStateHandler)(void const * _Nonnull, int32_t), void (* _Nonnull didDiscoverPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull), void (* _Nonnull didConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull), void (* _Nonnull didFailToConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDisconnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverServicesHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverCharacteristicsHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t));
 
 
 SWIFT_EXTERN void cb4u_central_manager_release(void const * _Nonnull centralPtr);
@@ -337,6 +338,9 @@ SWIFT_EXTERN void cb4u_central_manager_scan_for_peripherals(void const * _Nonnul
 
 
 SWIFT_EXTERN void cb4u_central_manager_stop_scan(void const * _Nonnull centralPtr);
+
+
+SWIFT_EXTERN int32_t cb4u_peripheral_discover_characteristics(void const * _Nonnull centralPtr, char const * _Nonnull peripheralId, char const * _Nonnull serviceId, char const * _Nullable const * _Nonnull characteristicUUIDs, int32_t characteristicUUIDsCount) SWIFT_WARN_UNUSED_RESULT;
 
 
 SWIFT_EXTERN int32_t cb4u_peripheral_discover_services(void const * _Nonnull centralPtr, char const * _Nonnull peripheralId, char const * _Nullable const * _Nonnull serviceUUIDs, int32_t serviceUUIDsCount) SWIFT_WARN_UNUSED_RESULT;
