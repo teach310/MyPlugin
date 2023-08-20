97a553e9040b753307aec0a48ffb0dd9efe1ad60
teach310 <okms310@yahoo.co.jp>
2023-08-20 21:17:41 +0900

    update plugins

--- Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
+++ Assets/SamplePlugin/Plugins/iOS/SamplePlugin.framework/Headers/SamplePlugin-Swift.h
@@ -317,6 +317,7 @@ SWIFT_CLASS("_TtC12SamplePlugin18CB4UCentralManager")
 - (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverServices:(NSError * _Nullable)error;
 - (void)peripheral:(CBPeripheral * _Nonnull)peripheral didDiscoverCharacteristicsForService:(CBService * _Nonnull)service error:(NSError * _Nullable)error;
 - (void)peripheral:(CBPeripheral * _Nonnull)peripheral didUpdateValueForCharacteristic:(CBCharacteristic * _Nonnull)characteristic error:(NSError * _Nullable)error;
+- (void)peripheral:(CBPeripheral * _Nonnull)peripheral didWriteValueForCharacteristic:(CBCharacteristic * _Nonnull)characteristic error:(NSError * _Nullable)error;
 @end
 
 
@@ -332,7 +333,7 @@ SWIFT_EXTERN BOOL cb4u_central_manager_is_scanning(void const * _Nonnull central
 SWIFT_EXTERN void * _Nonnull cb4u_central_manager_new(void) SWIFT_WARN_UNUSED_RESULT;
 
 
-SWIFT_EXTERN void cb4u_central_manager_register_handlers(void const * _Nonnull centralPtr, void (* _Nonnull didUpdateStateHandler)(void const * _Nonnull, int32_t), void (* _Nonnull didDiscoverPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull), void (* _Nonnull didConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull), void (* _Nonnull didFailToConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDisconnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverServicesHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverCharacteristicsHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didUpdateValueForCharacteristicHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, uint8_t const * _Nonnull, int32_t, int32_t));
+SWIFT_EXTERN void cb4u_central_manager_register_handlers(void const * _Nonnull centralPtr, void (* _Nonnull didUpdateStateHandler)(void const * _Nonnull, int32_t), void (* _Nonnull didDiscoverPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull), void (* _Nonnull didConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull), void (* _Nonnull didFailToConnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDisconnectPeripheralHandler)(void const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverServicesHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didDiscoverCharacteristicsHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t), void (* _Nonnull didUpdateValueForCharacteristicHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, uint8_t const * _Nonnull, int32_t, int32_t), void (* _Nonnull didWriteValueForCharacteristicHandler)(void const * _Nonnull, char const * _Nonnull, char const * _Nonnull, char const * _Nonnull, int32_t));
 
 
 SWIFT_EXTERN void cb4u_central_manager_release(void const * _Nonnull centralPtr);
@@ -351,6 +352,9 @@ SWIFT_EXTERN int32_t cb4u_peripheral_discover_services(void const * _Nonnull cen
 
 
 SWIFT_EXTERN int32_t cb4u_peripheral_read_value_for_characteristic(void const * _Nonnull centralPtr, char const * _Nonnull peripheralId, char const * _Nonnull serviceId, char const * _Nonnull characteristicId) SWIFT_WARN_UNUSED_RESULT;
+
+
+SWIFT_EXTERN int32_t cb4u_peripheral_write_value_for_characteristic(void const * _Nonnull centralPtr, char const * _Nonnull peripheralId, char const * _Nonnull serviceId, char const * _Nonnull characteristicId, uint8_t const * _Nonnull dataBytes, int32_t dataLength, int32_t writeType) SWIFT_WARN_UNUSED_RESULT;
 
 
 SWIFT_EXTERN int32_t sample_plugin_helloworld(void) SWIFT_WARN_UNUSED_RESULT;
